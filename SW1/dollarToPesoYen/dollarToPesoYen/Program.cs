f4e4aa549cb4b4ad54740cb5878b1dd193cc1475
Fernandez-Cjj <cf9407040@gmail.com>
2025-09-15 14:13:29 +0800

    SW1

---
+++ SW1/dollarToPesoYen/dollarToPesoYen/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dollarToPesoYen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter currency in ($): ");
+            string input = Console.ReadLine();
+
+            int[] arrnum = input.Split(',').Select(int.Parse).ToArray();
+
+            Console.WriteLine($"{"Dollars($)",-12}{"Phil Peso(P)",-15}{"Jpn Yen(y)",-15}");
+            for(int c=0; c<arrnum.Length; c++)
+            {
+                (double peso, double yen) = convertCurrency(arrnum[c]);
+                Console.WriteLine($"{arrnum[c],-12}{peso,-15:F2}{yen,-15:F2}");
+            }
+
+            Console.ReadKey();
+        }
+
+        static (double peso, double yen) convertCurrency(int dollar)
+        {
+            double peso = dollar * 57.17;
+            double yen = dollar * 146.67;
+
+            return (peso, yen);
+
+        }
+    }
+}
---
+++ SW1/primeOrComposite/primeOrComposite/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace primeOrComposite
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter string: ");
+            string input = Console.ReadLine();
+
+            primeOrComposite(input);
+
+           
+            Console.ReadKey();
+        }
+
+        static void primeOrComposite(string arr)
+        {
+            int[] num = arr.Split(' ').Select(int.Parse).ToArray();
+            
+            for(int c=0; c<num.Length; c++)
+            {   
+                if(num[c] <= 1)
+                {
+                    Console.WriteLine($"{c + 1}. {num[c],-5} Neither prime nor composite");
+                    continue;
+                }
+                int factorCount = 0;
+                for(int i=1; i <= num[c]; i++)
+                {
+                    if(num[c] % i == 0)
+                    {
+                        factorCount++;
+                    }
+                }
+
+                if (factorCount == 2)
+                    Console.WriteLine($"{c + 1}. {num[c],-5} Prime");
+                else
+                    Console.WriteLine($"{c + 1}. {num[c],-5} Composite");
+            }
+
+
+        }
+    }
+}
